01acf233b77da83429b2669c0145644b19ab7409
Mohammad Ali Jaffri <malijaffri@posteo.com>
2026-05-07 17:28:21 +0500

    Remove dynamic colours but keep dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/welcome_screen.dart';
@@ -10,30 +9,19 @@ void main() {
 class FccuSocietiesHubApp extends StatelessWidget {
   const FccuSocietiesHubApp({super.key});
 
-  static const bool useDynamic = false;
-
   @override
-  Widget build(BuildContext context) => DynamicColorBuilder(
-    builder: (lightDynamic, darkDynamic) {
-      final ColorScheme lightScheme;
-      final ColorScheme darkScheme;
-
-      if (useDynamic && lightDynamic != null && darkDynamic != null) {
-        lightScheme = lightDynamic;
-        darkScheme = darkDynamic;
-      } else {
-        lightScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.light);
-        darkScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark);
-      }
-
-      return MaterialApp(
-        title: 'FCCU Societies Hub',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(useMaterial3: true, colorScheme: lightScheme),
-        darkTheme: ThemeData(useMaterial3: true, colorScheme: darkScheme),
-        themeMode: ThemeMode.system,
-        home: const WelcomeScreen(),
-      );
-    },
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'FCCU Societies Hub',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: .fromSeed(seedColor: Colors.deepPurple, brightness: .light),
+      useMaterial3: true,
+    ),
+    darkTheme: ThemeData(
+      colorScheme: .fromSeed(seedColor: Colors.deepPurple, brightness: .dark),
+      useMaterial3: true,
+    ),
+    themeMode: .system,
+    home: const WelcomeScreen(),
   );
 }
