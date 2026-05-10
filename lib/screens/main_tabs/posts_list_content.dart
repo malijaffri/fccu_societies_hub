e2d39f2e259cd549f6681924322a4bb3c435121d
Mohammad Ali Jaffri <malijaffri@posteo.com>
2026-05-10 21:01:30 +0500

    Use new `PostCard` widget

--- lib/screens/main_tabs/posts_list_content.dart
+++ lib/screens/main_tabs/posts_list_content.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../../widgets/post_card.dart';
+import 'package:fccu_societies_hub/features/feed/post_card.dart';
+import 'package:fccu_societies_hub/models/post.dart';
 
 class PostsListContent extends StatelessWidget {
   const PostsListContent({super.key});
 
   @override
-  Widget build(BuildContext context) => ListView.builder(
-    padding: const EdgeInsets.all(16),
-    itemCount: 3, // TODO: Mock data count
-    itemBuilder: (context, index) => PostCard(
-      posterName: 'Society Name',
-      postedAt: DateTime.now(),
-      content: 'Post Description\nWith multiple lines\nAnd potentially rich text.',
-      imageUrls: [],
-      favorites: 1_000_000,
-      comments: 92,
-      bookmarks: 1_400,
-    ),
-  );
+  Widget build(BuildContext context) {
+    final posts = mockPosts();
+
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: posts.length,
+      itemBuilder: (context, index) => PostCard(post: posts[index], onTap: () {}, onLike: () {}, onComment: () {}),
+    );
+  }
 }
